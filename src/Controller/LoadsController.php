280cbdbbf8d568a4b068578be660b7a15a376fab
jonathan789 <jcruz2@udi.edu.co>
2023-05-27 11:42:26 -0500

    testeo

--- src/Controller/LoadsController.php
+++ src/Controller/LoadsController.php
@@ -400,8 +400,7 @@ class LoadsController extends AbstractController
             "messages" => array(),
             "data" => array()
         );
-        dump($excel_row);
-        die();
+        
 
         if ( is_numeric($excel_row[11])){
             $fecha = \PhpOffice\PhpSpreadsheet\Shared\Date::excelToDateTimeObject($excel_row[11]);
